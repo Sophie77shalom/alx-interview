e08b048a93f5896a9dcb2d0d989d6c57ecba5e91
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-04 22:03:32 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle """
+"""Task 0. """
 
 
 def look_next_opened_box(opened_boxes):
-    """Looks for the next opened box
-    Args:
-        opened_boxes (dict): Dictionary which contains boxes already opened
-    Returns:
-        list: List with the keys contained in the opened box
+    """
+    Looks for the next opened box
+
     """
     for index, box in opened_boxes.items():
         if box.get('status') == 'opened':
@@ -17,11 +15,9 @@ def look_next_opened_box(opened_boxes):
 
 
 def canUnlockAll(boxes):
-    """Check if all boxes can be opened
-    Args:
-        boxes (list): List which contain all the boxes with the keys
-    Returns:
-        bool: True if all boxes can be opened, otherwise, False
+    """
+    Check if all boxes can be opened
+
     """
     if len(boxes) <= 1 or boxes == [[]]:
         return True
@@ -63,3 +59,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
