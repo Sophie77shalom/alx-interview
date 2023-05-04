05a19533b03087be0a5436925235515aeaf137bf
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-04 22:07:46 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,62 +1,16 @@
-#!/usr/bin/python3
-"""Task 0. """
-
-
-def look_next_opened_box(opened_boxes):
-    """
-    Looks for the next opened box
-
-    """
-    for index, box in opened_boxes.items():
-        if box.get('status') == 'opened':
-            box['status'] = 'opened/checked'
-            return box.get('keys')
-    return None
-
-
 def canUnlockAll(boxes):
-    """
-    Check if all boxes can be opened
+    num_boxes = len(boxes)
+    unlocked_boxes = [False] * num_boxes
+    unlocked_boxes[0] = True  # first box is always unlocked
+    stack = [0]  # start with first box
 
-    """
-    if len(boxes) <= 1 or boxes == [[]]:
-        return True
+    while stack:
+        box_num = stack.pop()
+        for key in boxes[box_num]:
+            if key >= num_boxes:  # invalid key
+                continue
+            if not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                stack.append(key)
 
-    aux = {}
-    while True:
-        if len(aux) == 0:
-            aux[0] = {
-                'status': 'opened',
-                'keys': boxes[0],
-            }
-        keys = look_next_opened_box(aux)
-        if keys:
-            for key in keys:
-                try:
-                    if aux.get(key) and aux.get(key).get('status') \
-                       == 'opened/checked':
-                        continue
-                    aux[key] = {
-                        'status': 'opened',
-                        'keys': boxes[key]
-                    }
-                except (KeyError, IndexError):
-                    continue
-        elif 'opened' in [box.get('status') for box in aux.values()]:
-            continue
-        elif len(aux) == len(boxes):
-            break
-        else:
-            return False
-
-    return len(aux) == len(boxes)
-
-
-def main():
-    """Entry point"""
-    canUnlockAll([[]])
-
-
-if __name__ == '__main__':
-    main()
-    
+    return all(unlocked_boxes)
